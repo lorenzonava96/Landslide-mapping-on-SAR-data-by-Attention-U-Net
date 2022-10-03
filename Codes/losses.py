a3b07214b21f54c08eb0227b5cf9de661a8fb251
lorenzonava96 <89015253+lorenzonava96@users.noreply.github.com>
2022-10-03 21:26:50 +0200

    Add files via upload

---
+++ Codes/losses.py
@@ -0,0 +1,15 @@
+import tensorflow.keras.backend as K
+
+smooth = 1
+
+def dsc(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dsc(y_true, y_pred)
+    return loss
